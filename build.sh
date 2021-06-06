81a3f472d2c5383f9e11dd67ff066db5ed9f1bc4
Emanuele Bardelli <bardellie@gmail.com>
2021-06-06 15:57:13 -0400

    Make build script more general

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-hash=`npx ipfs-deploy public/ -p pinata | tail -1`
-curl https://www.duckdns.org/update\?domains\=$DUCKDNS_DOMAIN\&token\=$DUCKDNS_TOKEN\&txt\=dnslink\=/ipfs/$hash
 
+if [[ ! -z "${PINNING_SERVICE}" ]]; then
+    PIN="-p $PINNING_SERVICE"
+else
+    PIN="-p infura"
+fi
+
+if [[ ! -z "${DNS_SERVICE}" ]]; then
+    DSN="-d $DNS_SERVICE"
+else
+    DNS=""
+fi
+
+hash=`npx ipfs-deploy $SOURCE $PIN $DNS | tail -1`
 printf "\nIPFS root hash: $hash"
+echo $hash >> .hash
